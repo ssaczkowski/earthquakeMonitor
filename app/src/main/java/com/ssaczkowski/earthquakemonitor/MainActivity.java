2a3b63dece59acafd6ebba57ba5a893f0c57d0bf
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2018-07-29 20:29:59 -0300

    Add files via upload

---
+++ app/src/main/java/com/ssaczkowski/earthquakemonitor/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ssaczkowski.earthquakemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ListView earthquakeListView = (ListView) findViewById(R.id.earthquake_list_view);
+        ArrayList<String> countriesList = new ArrayList<>();
+
+        countriesList.add("Argentina");
+        countriesList.add("México");
+        countriesList.add("Venezuela");
+        countriesList.add("Colombia");
+        countriesList.add("España");
+        countriesList.add("Perú");
+
+        ArrayAdapter<String> countriesAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, countriesList);
+        earthquakeListView.setAdapter(countriesAdapter);
+    }
+}
